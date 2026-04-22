ea7f47bceb81acb8be4b71271ebabe09a68c1f32
nickkea05 <nicholas.a.kea@vanderbilt.edu>
2026-04-22 20:41:37 +0200

    tc03 passing - raise exception when file is not valid json

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,12 +1,11 @@
 """Module """
-import hashlib
 import json
 import os
 import re
-from datetime import datetime
 from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
 from uc3m_consulting.enterprise_project import EnterpriseProject
 from uc3m_consulting.project_document import ProjectDocument
+
 
 class EnterpriseManager:
     """Class for providing the methods for managing the orders"""
@@ -15,11 +14,15 @@ class EnterpriseManager:
 
     @staticmethod
     def validate_cif(cif: str):
-        """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
-        OR FALSE IN OTHER CASE"""
+        """Returns True if the CIF received is valid, False otherwise"""
         return True
 
     def register_document(self, input_file: str):
         """Registers a document for a project"""
         if not os.path.exists(input_file):
             raise EnterpriseManagementException("Input file not found")
+        try:
+            with open(input_file, "r", encoding="utf-8") as f:
+                data = json.load(f)
+        except json.JSONDecodeError as exc:
+            raise EnterpriseManagementException("File is not valid JSON") from exc
